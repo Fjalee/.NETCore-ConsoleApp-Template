32c58f61215b6649a19a56aaf67cac5c449674f3
Rytis Cepulis <cepRytis@gmail.com>
2022-10-08 22:36:44 +0300

    catching and logging unhandled exceptions

--- Template/Template/Program.cs
+++ Template/Template/Program.cs
@@ -9,13 +9,24 @@ namespace Template
 {
     public class Program
     {
+        private static ILogger<Program>? _logger;
+
+        static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
+        {
+            _logger?.LogCritical("Unhandled exception occured\n" + (Exception)e.ExceptionObject);
+            throw new Exception("Global Exception Handler");
+        }
+
         static void Main()
         {
+            AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
+
             var services = new ServiceCollection();
             ConfigureServices(services);
             var serviceProvider = services.BuildServiceProvider();
 
-            //serviceProvider.GetService<IExampleClass>();             //delete this line (its from template) 
+            _logger = serviceProvider.GetService<ILogger<Program>>();
+
             //var config = serviceProvider.GetService<IOptions<AppSettingsOptions>>()?.Value;       //delete this line (its from template)
             //var testValFromConfig = config?.TestObject?.TestValue;     //delete this line (its from template)
         }
