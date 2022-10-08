2a021fdb2fb37d5502eb7f16ae22e22fa2e4588b
Rytis Cepulis <cepRytis@gmail.com>
2022-10-08 22:07:18 +0300

    configuring dependency injection

--- Template/Template/Program.cs
+++ Template/Template/Program.cs
@@ -6,6 +6,26 @@ namespace Template
     {
         static void Main()
         {
+            var services = new ServiceCollection();
+            ConfigureServices(services);
+            var serviceProvider = services.BuildServiceProvider();
+
+            //serviceProvider.GetService<IExample>();             //getting example class implementation
+        }
+
+        private static void ConfigureServices(IServiceCollection services)
+        {
+            var config = SetupConfiguration();
+
+            //services
+            //    .AddTransient<IExampleClass, ExampleClass>();             //injecting example class implementation
+        }
+
+        private static IConfiguration SetupConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
         }
     }
 }
