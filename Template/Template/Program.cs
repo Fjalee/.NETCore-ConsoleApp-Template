4c04966d8a35362b6a0397d98d88daa48130025c
Rytis Cepulis <cepRytis@gmail.com>
2024-01-17 00:16:40 +0200

    better to throw if logger is null

--- Template/Template/Program.cs
+++ Template/Template/Program.cs
@@ -6,11 +6,11 @@ namespace Template
 {
     public class Program
     {
-        private static ILogger<Program>? _logger;
+        private static ILogger<Program> _logger;
 
         static void UnhandledExceptionTrapper(object sender, UnhandledExceptionEventArgs e)
         {
-            _logger?.LogCritical("Unhandled exception occured\n" + (Exception)e.ExceptionObject);
+            _logger.LogCritical("Unhandled exception occured\n" + (Exception)e.ExceptionObject);
             throw new Exception("Global Exception Handler");
         }
 
