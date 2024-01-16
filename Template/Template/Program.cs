c902edb5a180a42fc029227c3546e004190b2356
Rytis Cepulis <cepRytis@gmail.com>
2024-01-16 22:59:35 +0200

    SOC

--- Template/Template/Program.cs
+++ Template/Template/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using NLog;
 using NLog.Extensions.Logging;
+using Youtube_Playlist_Manager;
 
 namespace Template
 {
@@ -21,45 +22,14 @@ namespace Template
         {
             AppDomain.CurrentDomain.UnhandledException += UnhandledExceptionTrapper;
 
-            var services = new ServiceCollection();
-            ConfigureServices(services);
+            var services = ServiceConfigurator.ConfigureServices(
+                ServiceConfigurator.SetupConfiguration());
             var serviceProvider = services.BuildServiceProvider();
 
             _logger = serviceProvider.GetService<ILogger<Program>>();
 
             //var config = serviceProvider.GetService<IOptions<AppSettingsOptions>>()?.Value;       //delete this line (its from template)
             //var testValFromConfig = config?.TestObject?.TestValue;     //delete this line (its from template)
-        }
-
-        private static void ConfigureServices(IServiceCollection services)
-        {
-            var config = SetupConfiguration();
-
-            services
-                .AddAutoMapper(typeof(Program))
-                .Configure<AppSettingsOptions>(
-                    config.GetSection("AppSettings"))
-                .Configure<TestObjectOptions>(
-                    config.GetSection("TestObject"));
-
-            services.AddLogging(builder =>
-            {
-                builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                builder.AddNLog("nlog.config");
-            });
-            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
-
-            //services
-            //.AddTransient<IExampleClass, ExampleClass>();             //delete this line (its from template) 
-        }
-
-        private static IConfiguration SetupConfiguration()
-        {
-            return new ConfigurationBuilder()
-                .AddJsonFile($"local-appsettings.json", false)
-                .AddJsonFile($"appsettings.json", false)
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .Build();
         }
     }
 }
---
+++ Template/Template/ServiceConfigurator.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using NLog.Extensions.Logging;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+using Template;
+
+namespace Youtube_Playlist_Manager
+{
+    public static class ServiceConfigurator
+    {
+        public static IServiceCollection ConfigureServices(IConfiguration config)
+        {
+            var services = new ServiceCollection();
+
+            services
+                .AddAutoMapper(typeof(Program))
+                .Configure<AppSettingsOptions>(
+                    config.GetSection("AppSettings"))
+                .Configure<TestObjectOptions>(
+                    config.GetSection("TestObject"));
+
+            services.AddLogging(builder =>
+            {
+                builder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                builder.AddNLog("nlog.config");
+            });
+            LogManager.Configuration = new NLogLoggingConfiguration(config.GetSection("NLog"));
+
+            //services
+            //.AddTransient<IExampleClass, ExampleClass>();             //delete this line (its from template) 
+
+            return services;
+        }
+        
+        public static IConfiguration SetupConfiguration()
+        {
+            return new ConfigurationBuilder()
+                .AddJsonFile($"local-appsettings.json", false)
+                .AddJsonFile($"appsettings.json", false)
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .Build();
+        }
+    }
+}
